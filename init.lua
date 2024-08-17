dff28c84967fc7f5766c93996f20874398835091
bryonverdone <bryonverdone@gmail.com>
2024-08-17 09:18:27 -0400

    added neotree

--- init.lua
+++ init.lua
@@ -23,11 +23,25 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Add plugins
 local plugins = {
 	{"catppuccin/nvim", name = "catppuccin", priority = 1000},
 	{'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		dependencies = {'nvim-lua/plenary.nvim'}
-},{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}
+},
+
+{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}
 }
 
 local opts = {}
@@ -54,6 +68,8 @@ local config = require('nvim-treesitter.configs')
 })
 vim.cmd.colorscheme "catppuccin"
 
+-- Neotree key maps
 
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>', {})
 
 
